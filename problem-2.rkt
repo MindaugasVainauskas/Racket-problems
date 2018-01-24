2e862b9c4bebb2b4c2a738af7899cb31cc4e3db3
M.Vainauskas <mindelis1@gmail.com>
2018-01-24 12:39:23 +0000

    Added title to problem 2 file

--- problem-2.rkt
+++ problem-2.rkt
@@ -2,3 +2,5 @@
 
 ;This is a file to solve problem No.2 from Racket problem sheet
 ; Student: Mindaugas Vainauskas. Year 4, Software Development course. GMIT 2018
+
+'(Problem 2. Return discounted price from procedure, taking price and discount precentage as inputs.)
